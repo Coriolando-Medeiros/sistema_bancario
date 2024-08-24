e12806d40bf541d53228084ef48687cced4f76cf
Coriolando <coriolando.medeiros@gmail.com>
2024-08-24 08:51:33 -0300

    feat(cadastrar): lógica de criar conta

--- banco.rb
+++ banco.rb
@@ -1,21 +1,67 @@
+class Cliente
+
+  attr_accessor :nome, :cpf, :enderecos, :contas
+
+  def initialize
+    @nome = nil
+    @cpf = nil
+    @titular = []
+    @enderecos = []
+    @contas = []
+  end
+
+  def cadastar
+    puts "Nome"
+    nome_cadastro = gets.chomp
+    puts "CPF"
+    cpf_cadastro = gets.chomp
+    @titular << { nome: nome_cadastro, cpf: cpf_cadastro }
+  end
+
+  def adicionar_endereco
+    puts "Endereço"
+    endereco = gets.chomp.capitalize
+    puts "Cidade"
+    cidade = gets.chomp.capitalize
+    puts "Estado"
+    estado = gets.chomp.capitalize
+    @enderecos << { endereco: endereco, cidade: cidade, estado: estado }
+  end
+
+  def adicionar_conta
+    puts
+  end
+
+  def exibir_contas
+    puts
+  end
+
+end
+
+
 class ContaBancaria
   attr_accessor :numero_conta, :saldo, :titular, :historico_transações
 
   def initialize
-    @numero_conta = numero
+    @numero_conta = @numero
     @saldo = 0
-    @titular = []
     @historico_transações = []
   end
   def criar_conta
-    if Cliente
-      puts "Nome: #{@titular.nome}, CPF: #{@titular.cpf}"
+    if @titular
+      titular = @titular
+      titulares.each do |titular|
+        puts "Nome: #{titular.nome}, CPF: #{titular.cpf}"
+      end
+      
+      
       puts "Numero da conta"
-      numero = gets.chomp.to_i
+      @numero = gets.chomp.to_i
       puts "Saldo inicial R$ #{@saldo}"
       puts "Para a conta ser aberta, é necesário"
       puts "fazer um deposito mninimo de R$ 50,00"
       depositar
+
     end
 
   end
@@ -55,45 +101,6 @@ class ContaBancaria
 
 end
 
-class Cliente
-
-  attr_accessor :nome, :cpf, :enderecos, :contas
-
-  def initialize
-    @nome = nome
-    @cpf = cpf
-    @enderecos = []
-    @contas = []
-  end
-
-  def cadastar
-    puts "Nome"
-    nome_cadastro = gets.chomp.capitalize
-    puts "CPF"
-    cpf_cadastro = gets.chomp
-    @titular << { nome: nome_cadastro, cpf: cpf_cadastro }
-  end
-
-  def adicionar_endereco
-    puts "Endereço"
-    endereco = gets.chomp.capitalize
-    puts "Cidade"
-    cidade = gets.chomp.capitalize
-    puts "Estado"
-    estado = gets.chomp.capitalize
-    @enderecos << { endereco: endereco, cidade: cidade, estado: estado }
-  end
-
-  def adicionar_conta
-    puts
-  end
-
-  def exibir_contas
-    puts
-  end
-
-end
-
 class Transacao
 
   attr_accessor :data, :tipo, :valor, :conta_origem, :conta_destino
@@ -111,7 +118,9 @@ class Transacao
   end
 end
 
-minha_conta = ContaBancaria.new
+
 meu_cliente = Cliente.new
 meu_cliente.cadastar
 meu_cliente.adicionar_endereco
+minha_conta = ContaBancaria.new
+minha_conta.criar_conta
